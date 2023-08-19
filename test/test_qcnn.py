9512eb3f618da909a2a027c837057700ae6ba402
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-08-20 02:25:07 +0900

    add more tests

--- test/test_qcnn.py
+++ test/test_qcnn.py
@@ -15,16 +15,133 @@ from qiskit.circuit import ParameterVector
 
 from qmlant.models.binary_classification import SimpleQCNN
 from qmlant.neural_networks import (
-    EstimatorTN,
+    Ry_Rydag,
+    Rz_Rzdag,
     circuit_to_einsum_expectation,
     replace_by_batch,
     replace_pauli,
 )
-from qmlant.transforms import MapLabel, ToTensor
 
 
 class TestQCNN(unittest.TestCase):
-    def test_ansatz(self):
+    ZERO = cp.array([1, 0], dtype=complex)
+    X = cp.array([[0, 1], [1, 0]], dtype=complex)
+    Y = cp.array([[0, -1.0j], [1.0j, 0]], dtype=complex)
+    Z = cp.array([[1, 0], [0, -1]], dtype=complex)
+    H = cp.array([[1, 1], [1, -1]], dtype=complex) / np.sqrt(2)
+    CX = cp.array([[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0]], dtype=complex).reshape(
+        2, 2, 2, 2
+    )
+    DUMMY = cp.array([[0, 0], [0, 0]], dtype=complex)
+
+    @classmethod
+    def naive_Ry(cls, theta):
+        return cp.array(
+            [
+                [np.cos(theta / 2), -np.sin(theta / 2)],
+                [np.sin(theta / 2), np.cos(theta / 2)],
+            ],
+            dtype=complex,
+        )
+
+    @classmethod
+    def naive_Rz(cls, theta):
+        return cp.array(
+            [
+                [np.cos(theta / 2) - np.sin(theta / 2) * 1j, 0],
+                [0, np.cos(theta / 2) + np.sin(theta / 2) * 1j],
+            ],
+            dtype=complex,
+        )
+
+    def test_ansatz2(self):
+        ansatz = SimpleQCNN._make_ansatz(2, insert_barrier=True)
+        param_len = ansatz.num_parameters
+        params = [p for p in np.arange(0.01, np.pi, np.pi / param_len)][:param_len]
+        _, _, params2locs = circuit_to_einsum_expectation(ansatz, "IZ")
+        ansatz = ansatz.bind_parameters(params)
+        converter = CircuitToEinsum(ansatz)
+        expr, operands = converter.expectation("IZ")
+
+        answer_expr = "a,b,cb,deac,fd,ge,higf,jh,klij,mk,nm,opln,qo,rp,strq,us,vwtu,xv,yx,zAwy,Bz,CA,DECB,FD,GF,HG,IJHE,KI,LJ,MNLK,ON,PO,QRPM,SR,TUSQ,VT,WU,XYWV,ZY,ÀZ,ÁÂÀX,ÃÂ,ÄÅÃÁ,ÆÄ,ÇÅ,ÈÉÇÆ,ÊÉ,È,Ê->"
+        answer_operands = [
+            self.ZERO,
+            self.ZERO,
+            self.naive_Rz(-np.pi / 2),
+            self.CX,
+            self.naive_Rz(params[0]),
+            self.naive_Ry(params[1]),
+            self.CX,
+            self.naive_Ry(params[2]),
+            self.CX,
+            self.naive_Rz(np.pi / 2),
+            #####
+            self.naive_Rz(-np.pi / 2),
+            self.CX,
+            self.naive_Rz(params[3]),
+            self.naive_Ry(params[4]),
+            self.CX,
+            self.naive_Ry(params[5]),
+            self.CX,
+            self.naive_Rz(np.pi / 2),
+            #####
+            self.naive_Rz(-np.pi / 2),
+            self.CX,
+            self.naive_Rz(params[6]),
+            self.naive_Ry(params[7]),
+            self.CX,
+            self.naive_Ry(params[8]),
+            #####
+            self.Z,
+            #####
+            self.naive_Ry(-params[8]),
+            self.CX,
+            self.naive_Ry(-params[7]),
+            self.naive_Rz(-params[6]),
+            self.CX,
+            self.naive_Rz(np.pi / 2),
+            #####
+            self.naive_Rz(-np.pi / 2),
+            self.CX,
+            self.naive_Ry(-params[5]),
+            self.CX,
+            self.naive_Ry(-params[4]),
+            self.naive_Rz(-params[3]),
+            self.CX,
+            self.naive_Rz(np.pi / 2),
+            #####
+            self.naive_Rz(-np.pi / 2),
+            self.CX,
+            self.naive_Ry(-params[2]),
+            self.CX,
+            self.naive_Ry(-params[1]),
+            self.naive_Rz(-params[0]),
+            self.CX,
+            self.naive_Rz(np.pi / 2),
+            self.ZERO,
+            self.ZERO,
+        ]
+        answer_params2locs = {
+            "c1[0]": ([4], [44], Rz_Rzdag),
+            "c1[1]": ([5], [43], Ry_Rydag),
+            "c1[2]": ([7], [41], Ry_Rydag),
+            "c1[3]": ([12], [36], Rz_Rzdag),
+            "c1[4]": ([13], [35], Ry_Rydag),
+            "c1[5]": ([15], [33], Ry_Rydag),
+            "p1[0]": ([20], [28], Rz_Rzdag),
+            "p1[1]": ([21], [27], Ry_Rydag),
+            "p1[2]": ([23], [25], Ry_Rydag),
+        }
+
+        self.assertEqual(expr, answer_expr)
+        self.assertEqual(len(operands), len(answer_operands))
+        for ops, ops_ans in zip(operands, answer_operands):
+            if cp.all(ops_ans == self.DUMMY):
+                continue
+            self.assertTrue(cp.all(ops == ops_ans), (ops, ops_ans))
+        self.assertEqual(params2locs, answer_params2locs)
+
+    def test_ansatz8(self):
         for decompose in [False, True]:
             with self.subTest(decompose=decompose):
                 # make effect of barriers to a circuit be same as that of Qiskit codes
